026e6dd2591925b39ede128bc119aaaf0adeca6d
danigelabert <73782173+danigelabert@users.noreply.github.com>
2023-05-22 09:20:58 +0200

    API NOTICIES FETA

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,6 +14,7 @@ import {RestaurarComponent} from "./restaurar/restaurar.component";
 import {DatosComponent} from "./datos/datos.component";
 import {FormulariconsultesComponent} from "./formulariconsultes/formulariconsultes.component";
 import {GraficsComponent} from "./grafics/grafics.component";
+import {InfoappleComponent} from "./infoapple/infoapple.component";
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/pagina-web', pathMatch: 'full'},
@@ -31,7 +32,8 @@ const appRoutes: Routes = [
   {path:'canvi', component:  CambiarContrasenyaComponent},
   {path:'datos', component: DatosComponent},
   {path:'formulariconsultes', component: FormulariconsultesComponent},
-  {path:'grafics', component: GraficsComponent}
+  {path:'grafics', component: GraficsComponent},
+  {path:'infoapple', component: InfoappleComponent}
 ]
 
 export const routing= RouterModule.forRoot(appRoutes)
